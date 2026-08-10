867c6b6005495a35bb04a09ce5bd9ecffa4d7cc7
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-11 00:15:34 +0530

    make server file

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-const express = require("express");
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT||5000
+
+app.use('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
