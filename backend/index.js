a79e41058fcef963d6c06c171d81f4301db10318
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-11 23:46:05 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT||5000
+
 app.use(express.json());
 app.use(cors());
 database();
 
-const port = process.env.PORT||5000
 
 app.use('/', (req, res) => {
   res.send('Hello World!')
