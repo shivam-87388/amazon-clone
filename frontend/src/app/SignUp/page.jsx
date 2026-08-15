4054811d6213873db984eb2285d0a7948a7ab006
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-16 00:39:14 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -4,15 +4,19 @@ import React from 'react'
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div className="bg-cover bg-no-repeat bg-url[(/rectangle.png)]">
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
-      <h1>lorem</h1>
+      <div style={{ backgroundImage: "url('/rectangle.png')" }} className=" bg-cover bg-no-repeat bg-center">
+      <form className="flex flex-col items-center justify-center px-8 py-4">
+        <h1 className="font-bold font-['Lora'] text-white text-2xl ">Create Account</h1>
+        <label htmlFor='email' className="text-xl text-white">email</label>
+        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
+        <label htmlFor='email' className="text-xl text-white">email</label>
+        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
+        <label htmlFor='email' className="text-xl text-white">email</label>
+        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
+
+
+      </form>
+      
      
 
       
