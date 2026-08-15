3581457c2c956641aa33c54a9dd28407cbf4293b
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-15 23:37:03 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -4,7 +4,12 @@ import React from 'react'
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div className="bg-cover bg-no-repeat bg-[url(https://thumbs.dreamstime.com/b/incredibly-beautiful-sunset-sun-lake-sunrise-landscape-panorama-nature-sky-amazing-colorful-clouds-fantasy-design-115177001.jpg?w=992)]">
+      <div className="bg-cover bg-no-repeat bg-[url(frontend/stockimage/rectangle.png)]">
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
     <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
 
       </div>
