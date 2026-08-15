2f48bb79c0aa86e5c49b52a65758c646097b6f13
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-16 00:18:00 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -4,14 +4,18 @@ import React from 'react'
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div className="bg-cover bg-no-repeat bg-[url(frontend/stockimage/rectangle.png)]">
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
-    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
+      <div className="bg-cover bg-no-repeat bg-url[(/rectangle.png)]">
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+      <h1>lorem</h1>
+     
 
+      
       </div>
     
       
