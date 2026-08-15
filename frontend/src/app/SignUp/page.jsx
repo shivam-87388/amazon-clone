9050745aa3326178f62832c39f4d5fafcd3b4768
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-15 23:27:59 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div className="h-screen bg-no-repeat bg-[url(/rectangle.png)]
+      <div id="pic-style">
        ">
     <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci vitae eligendi laudantium ex quidem neque. Eius ex nostrum maiores ratione eaque temporibus nulla expedita, repellat cum. Consequuntur quo quas quis.</h1>
 
