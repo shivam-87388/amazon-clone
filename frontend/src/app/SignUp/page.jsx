63ac5cd03839e2a520d930660e902137a606f13c
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-16 21:46:17 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
 
-
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div style={{ backgroundImage: "url('/rectangle.png')" }} className=" bg-cover bg-no-repeat bg-center">
-      <form className="flex flex-col items-center justify-center px-8 py-4">
-        <h1 className="font-bold font-['Lora'] text-white text-2xl ">Create Account</h1>
-        <label htmlFor='email' className="text-xl text-white">email</label>
-        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
-        <label htmlFor='email' className="text-xl text-white">email</label>
-        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
-        <label htmlFor='email' className="text-xl text-white">email</label>
-        <input type="email" id="email" name="email" placeholder='shopping@gmail.com'></input>
-
+      <div style={{ backgroundImage: "url('/rectangle.png')" }} className=" bg-cover bg-no-repeat bg-center rounded-4xl">
+      <form className="flex flex-col  items-center justify-center px-8 py-4">
+        <h1 className="text-4xl font-bold font-['Lora'] text-white" >Create Account</h1>
+        <div className="flex flex-col px-12 py-4 gap-0.5">
+          {/* fullname field */}
+          <label htmlFor="fullName" className="text-white font-bold font-['Lora']">Full Name</label>
+          <input type="text" id="fullName" name="user_name"required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          {/* email field */}
+          <label htmlFor="userEmail" className="text-white font-bold font-['Lora']">Email</label>
+          <input type="email" id="userEmail" name="email_address" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          {/* password field */}
+          <label htmlFor="password" className="text-white font-bold font-['Lora']">Password</label>
+          <input type="password" id="password" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          {/* confirm password */}
+          <label htmlFor="confirmpassword" className="text-white font-bold font-['Lora']">Confirm Password</label>
+          <input type="password" id="confirmpassword" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+        </div>
+        
+        
+        
 
       </form>
       
