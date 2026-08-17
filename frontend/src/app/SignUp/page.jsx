b351df3aa564790d35d496b0fb0416756159b585
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-17 12:02:47 +0530

    add

--- frontend/src/app/SignUp/page.jsx
+++ frontend/src/app/SignUp/page.jsx
@@ -3,32 +3,32 @@ import React from 'react'
 const createAccount = () => {
   return (
     <div className="min-h-screen bg-white flex justify-center items-center">
-      <div style={{ backgroundImage: "url('/rectangle.png')" }} className=" bg-cover bg-no-repeat bg-center ">
+      <div style={{ backgroundImage: "url('/rectangle.png')" }} className=" bg-cover bg-no-repeat bg-center">
       <form className="flex flex-col items-center justify-center px-12 py-12">
         <h1 className="text-4xl font-bold font-['Lora'] text-white" >Create Account</h1>
         <div className="flex flex-col w-full px-2 py-4 gap-1">
           {/* fullname field */}
           <div className="flex flex-col text-white font-['Lora']">
           <label htmlFor="fullName" className=" font-bold">Full Name</label>
-          <input type="text" id="fullName" name="user_name"required className="px-2.5 py-1 border-2 border-white rounded-md  active:ring-none"></input>
+          <input type="text" id="fullName" name="user_name"required className="px-2.5 py-1 border-2 border-white rounded-md focus:outline-none"></input>
           </div>
           
           {/* email field */}
           <div className="flex flex-col">
           <label htmlFor="userEmail" className="text-white font-bold font-['Lora']">Email</label>
-          <input type="email" id="userEmail" name="email_address" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          <input type="email" id="userEmail" name="email_address" required className="px-2.5 py-1 border-2 border-white rounded-md focus:outline-none"></input>
           </div>
           
           {/* password field */}
           <div className="flex flex-col ">
- <label htmlFor="password" className="text-white font-bold font-['Lora']">Password</label>
-          <input type="password" id="password" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          <label htmlFor="password" className="text-white font-bold font-['Lora']">Password</label>
+          <input type="password" id="password" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md focus:outline-none"></input>
           </div>
          
           {/* confirm password */}
           <div className="flex flex-col">
-<label htmlFor="confirmpassword" className="text-white font-bold font-['Lora']">Confirm Password</label>
-          <input type="password" id="confirmpassword" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md"></input>
+          <label htmlFor="confirmpassword" className="text-white font-bold font-['Lora']">Confirm Password</label>
+          <input type="password" id="confirmpassword" name="user_password" required className="px-2.5 py-1 border-2 border-white rounded-md focus:outline-none"></input>
           </div>
           
         </div>
