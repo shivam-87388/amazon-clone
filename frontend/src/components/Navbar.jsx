7365348e0521fc65a6ad2240fc92c4d3b773e59e
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-23 13:10:12 +0530

    add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,9 +9,9 @@ const navbar = () => {
       <nav className="flex flex-row gap-6 justify-center">
         <a href="" className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">Home</a>
         <a href=""className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">product</a>
-        <a href=""className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">aboutus</a>
+        <a href=""className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">about</a>
       </nav>
-      <div className="flex gap-4 justify-end">
+      <div className="flex gap-4">
         <a href="#" className="flex-1 bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">signup</a>
         <a href="#" className="flex-1 bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">login</a>
         
