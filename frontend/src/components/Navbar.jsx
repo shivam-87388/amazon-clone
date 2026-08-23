8d9b85786bd8132caac29ca504f0283d640c9373
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-23 13:47:46 +0530

    add

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const navbar = () => {
   return (
-    <div className="flex flex-row py-4 px-4 items-center justify-between">
-      <div>
+    <div className="grid grid-cols-3 py-4 px-8 items-center">
+      <div className="flex items-center justify-start">
         <img src="./Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png" className="h-32 justify-start"></img>
       </div>
-      <nav className="flex flex-row gap-6 justify-center">
+      <nav className="flex items-center justify-center gap-12">
         <a href="" className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">Home</a>
         <a href=""className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">product</a>
         <a href=""className="text-lg text-teal-800 font-[lora] font-semibold hover:underline">about</a>
       </nav>
-      <div className="flex gap-4">
-        <a href="#" className="flex-1 bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">signup</a>
-        <a href="#" className="flex-1 bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">login</a>
+      <div className="flex items-center justify-end gap-4">
+        <a href="#" className="w-24 text-center bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">signup</a>
+        <a href="#" className="w-24 text-center bg-teal-800 text-white px-2.5 py-2 rounded-md text-lg font-medium ring-2 hover:ring-teal-800 hover:border-2 hover:border-white border-2 border-transparent">login</a>
         
 
       </div>
