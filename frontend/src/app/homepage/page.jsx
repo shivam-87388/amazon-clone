510ebfbfa2d2d2fed028608a17946f53fad64f4a
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-24 23:50:30 +0530

    add

--- frontend/src/app/homepage/page.jsx
+++ frontend/src/app/homepage/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
             <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D" className='w-64 h-64 rounded-lg'></img>
             <div className="flex flex-col py-1">
                 
-            <h1 className="text-lg leading-tight  ">Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+            <h1 className="text-lg leading-tight font-semibold text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
             <p className="mt-1.5">Lorem ipsum dolor sit amet.</p>
            
             </div>
