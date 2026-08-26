9ca7015f4efca4bfc4fac04ba334d52e914d0c1f
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-26 23:19:12 +0530

    add

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,8 +4,17 @@ import React from 'react'
 const footer = () => {
   return (
     <footer className="bg-[#011732] w-full h-80">
-        <div className="grid grid-cols-4 p-6 items-center ">
-            <nav className="flex justify-center items-center text-white font-md ">Navigation Link</nav>
+        <div className="grid grid-cols-4 items-center justify-around p-4">
+            <img src=""></img>
+            <nav className="flex flex-col justify-center items-center text-white font-md ">Navigation Link
+                <ul className="flex flex-col gap-1">
+                    <a href=''>home</a>
+                    <a href=''>Product</a>
+                    <a href=''>about</a>
+                    <a href=''>cart</a>
+                </ul>
+            </nav>
+            
         </div>
        
 
