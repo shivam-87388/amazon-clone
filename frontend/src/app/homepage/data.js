12e068bf4b7299c3ddf3e8a8a944a4574987cea5
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-26 12:40:20 +0530

    add

--- frontend/src/app/homepage/data.js
+++ frontend/src/app/homepage/data.js
@@ -1,7 +1,8 @@
 const products =[
     {id:1,
         name:"shivam",
-        desc:"this a shart"
+        desc:"this a shart",
+        pic:"https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D"
     },
     {id:2,
         name:"shiva",
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react'
+
+const footer = () => {
+  return (
+   <footer className="grid grid-cols-3 bg-[#011732] ">
+    <nav>
+        <ul>
+           <h1 className="flex items-center  justify-center text-white font-medium">about us</h1>
+        </ul>
+    </nav>
+     <nav>
+        <ul>
+            <li><a href='' className="text-white font-medium">about click shop</a></li>
+        </ul>
+    </nav>
+     <nav>
+        <ul>
+            <li><a href='' className="text-white font-medium">about click shop</a></li>
+        </ul>
+    </nav>
+
+   </footer>
+  )
+}
+
+export default footer
