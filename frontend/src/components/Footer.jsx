c59e52bcaec9ac53066846924e643f008bf9e889
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-28 00:05:23 +0530

    add

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,29 +3,35 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <footer className="flex items-center bg-[#011732] h-64 w-full">
-        <div className="grid grid-cols-4 w-full justify-center ">
+    <footer className="flex items-center bg-[#011732] h-64 justify-around w-full">
+        <div className="grid grid-cols-4 ">
             <div className="flex items-center justify-center">
              <img src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png"className="object-center object-contain w-40"/> 
             </div>
-            <nav className="flex flex-col text-center justify-center text-lg text-white font-medium h-full">Navagation Link
-            <ul className="flex flex-col gap-1.5">
-                <a>home</a>
-                <a>product</a>
-                <a>cart</a>
-            </ul>
+            <nav className="flex flex-col items-center text-lg text-white font-medium">Navagation Link
+                <ul className="flex flex-col items-center gap-1.5 mt-1.5">
+                    <a>home</a>
+                    <a>product</a>
+                    <a>about-us</a>
+                    <a>cart</a>
+                </ul>
             </nav>
-            <nav className="flex justify-center text-lg text-white font-medium">social Link</nav>
-            <ul>
-                <a></a>
-                <a></a>
-                <a></a>
-            </ul>
-            <nav className="flex justify-center text-lg text-white font-medium">Contact us
-                <a></a>
-                <a></a>
-                <a></a>
+           <nav className="flex flex-col items-center text-lg text-white font-medium">Social Link
+                <ul className="flex flex-col items-center gap-1.5 mt-1.5">
+                    <a>instagram</a>
+                    <a>facebook</a>
+                    <a>Youtube</a>
+                    <a></a>
+                </ul>
             </nav>
+           
+            <div className="flex flex-col items-end justify-end text-lg text-white font-medium">
+               <div className="flex flex-row gap-1.5">
+                <input className="w-64 border-2 border-white rounded-lg "></input>
+                <button className="text-md px-3.5 py-1.5 bg-[#FF8D28] rounded-lg font-semibold hover:cursor-pointer hover:bg-[#1DB5A7]">submit</button>
+
+               </div>
+            </div>
 
         </div>
     </footer>
