cb624e8438e5aece9b4fbb3fac64a49dff1ed9da
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-27 14:04:14 +0530

    add

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,21 +3,31 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <footer className="bg-[#011732] w-full h-80">
-        <div className="grid grid-cols-4 items-center justify-around p-4">
-            <img src=""></img>
-            <nav className="flex flex-col justify-center items-center text-white font-md ">Navigation Link
-                <ul className="flex flex-col gap-1">
-                    <a href=''>home</a>
-                    <a href=''>Product</a>
-                    <a href=''>about</a>
-                    <a href=''>cart</a>
-                </ul>
+    <footer className="flex items-center bg-[#011732] h-64 w-full">
+        <div className="grid grid-cols-4 w-full justify-center ">
+            <div className="flex items-center justify-center">
+             <img src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png"className="object-center object-contain w-40"/> 
+            </div>
+            <nav className="flex flex-col text-center justify-center text-lg text-white font-medium h-full">Navagation Link
+            <ul className="flex flex-col gap-1.5">
+                <a>home</a>
+                <a>product</a>
+                <a>cart</a>
+            </ul>
             </nav>
-            
-        </div>
-       
+            <nav className="flex justify-center text-lg text-white font-medium">social Link</nav>
+            <ul>
+                <a></a>
+                <a></a>
+                <a></a>
+            </ul>
+            <nav className="flex justify-center text-lg text-white font-medium">Contact us
+                <a></a>
+                <a></a>
+                <a></a>
+            </nav>
 
+        </div>
     </footer>
   )
 }
