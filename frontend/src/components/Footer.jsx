4db602bf4bf4caaae061a98dc4cc5f1fd15f6d08
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-29 22:29:10 +0530

    add

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,44 +3,39 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="flex items-center bg-[#011732] h-64 justify-around w-full">
-       
-        <div className="flex flex-row flex-wrap">
-            <div className="flex items-center justify-center">
-             <img src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png"className="object-center object-contain w-40" alt="brand logo"/> 
-            </div>
-            <nav className="flex flex-col items-center text-white">
-                <h3 className="text-lg ">Navagation Link</h3>
-                <ul className="flex flex-col items-center gap-1.5 mt-1.5 font-medium">
-                    <li><a href="/">home</a></li>
-                    <li><a href="/">product</a></li>
-                    <li><a href="/">about</a></li>
-                    <li><a href="/">cart</a></li>
-                    
-                </ul>
-            </nav>
-           <nav className="flex flex-col items-center  text-white ">
-            <h3 className="font-medium text-lg">Social Link</h3>
-                <ul className="flex flex-col items-center gap-1.5 mt-1.5 font-medium">
-                    <li><a href="/" className="hover:underline">facebook</a></li>
-                    <li><a href="/">instagram</a></li>
-                    <li><a href="/">Youtube</a></li>
-                </ul>
-            </nav>
-           
-            <div className="flex flex-col justify-end text-white font-medium">
-                <h3 className="px-1.5 py-1.5 text-lg font-semibold">subscribe</h3>
-               <form className="flex flex-row gap-2.5">
-                <input placeholder='email' className="px-1.5 py-1.5 w-64 border-2 border-white rounded-lg "></input>
-                <button className="text-center text-md px-3.5 py-1.5 bg-[#FF8D28] rounded-lg font-semibold hover:cursor-pointer hover:bg-[#1DB5A7] hover:ring-2 hover:ring-white">submit</button>
-
-               </form>
-            </div>
-
-        </div>
-        
-        
-    </footer>
+   <footer className="flex px-6 py-14 bg-slate-900  justify-around items-start flex-wrap content-start">
+  <img className="w-52 h-56" src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png"/>
+  <nav className="flex flex-col justify-center items-center gap-3">
+    <h3 className="felx justify-start text-white text-xl font-bold font-['Lora']">Navigation Link</h3>
+    <ul className="flex flex-col justify-center items-center gap-3.5  text-white text-xl font-normal font-['Lora']">
+      <li><a href='/'>home</a></li>
+      <li><a href='/'>product</a></li>
+      <li><a href='/'>about</a></li>
+      <li><a href='/'>cart</a></li>
+    </ul>
+  </nav>
+  {/* social links */}
+   <nav className="flex flex-col justify-center items-center gap-3">
+    <h3 className="felx justify-start text-white text-xl font-bold font-['Lora']">Navigation Link</h3>
+    <ul className="flex flex-col justify-center items-center gap-3.5  text-white text-xl font-normal font-['Lora']">
+      <li><a href='/'>home</a></li>
+      <li><a href='/'>product</a></li>
+      <li><a href='/'>about</a></li>
+      <li><a href='/'>cart</a></li>
+    </ul>
+  </nav>
+  <div className="h-24 flex flex-col justify-between items-center">
+    <div className="self-stretch px-2.5 py-1 flex justify-start items-center gap-2.5">
+      <div className="justify-start text-white text-xl font-bold font-['Lora']">Subscribe</div>
+    </div>
+    <div className="self-stretch flex justify-center items-center gap-3.5 flex-wrap content-center">
+      <input className="w-60 h-16 rounded-[10px] border-2 border-white"/>
+      <div className="px-6 py-3 bg-orange-400 rounded-[10px] flex justify-center items-center gap-2.5">
+        <div className="justify-start text-white text-xl font-bold font-['Lora']">submit</div>
+      </div>
+    </div>
+  </div>
+</footer>
   )
 }
 
