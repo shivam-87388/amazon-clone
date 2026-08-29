5610c2c738aca5d37f265465601150826d4979ed
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-08-30 00:22:10 +0530

    add

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,9 +4,13 @@ import React from 'react'
 const Footer = () => {
   return (
    <footer className="flex px-6 py-14 bg-slate-900  justify-around items-start flex-wrap content-start">
-  <img className="w-52 h-56" src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png"/>
-  {/* navagation link */}
-  <nav className="flex flex-col justify-center items-center gap-3">
+    <div className="w-full flex flex-row items-start justify-between gap-4">
+        <div className="flex-1 flex">
+
+         <img className="w-52 h-56" src="/Gemini_Generated_Image_e2nvm6e2nvm6e2nv.png" alt="brandimage"/>
+        </div>
+         {/* navagation link */}
+  <nav className=" flex-1 flex flex-col justify-center items-center gap-3">
     <h3 className="felx justify-center text-white text-xl font-bold font-['Lora']">Navigation Link</h3>
     <ul className="flex flex-col justify-center items-center gap-3.5  text-white text-xl font-normal font-['Lora']">
       <li><a href='/'>home</a></li>
@@ -16,26 +20,26 @@ const Footer = () => {
     </ul>
   </nav>
   {/* social links */}
-   <nav className="flex flex-col justify-center items-center gap-3">
+   <nav className="flex-1 flex flex-col items-center justify-center gap-3">
     <h3 className="felx justify-center text-white text-xl font-bold font-['Lora']">Social Link</h3>
     <ul className="flex flex-col justify-center items-center gap-3.5  text-white text-xl font-normal font-['Lora']">
-      <li><a href='/'>home</a></li>
-      <li><a href='/'>product</a></li>
-      <li><a href='/'>about</a></li>
-      <li><a href='/'>cart</a></li>
+      <li><a href='/'>facebook</a></li>
+      <li><a href='/'>instagram</a></li>
+      <li><a href='/'>youtube</a></li>
     </ul>
   </nav>
-  <div className="h-24 flex flex-col justify-between items-center">
-    <div className="self-stretch px-2.5 py-1 flex justify-start items-center gap-2.5">
-      <div className="justify-start text-white text-xl font-bold font-['Lora']">Subscribe</div>
+  {/* subscribe */}
+  <div className=" flex-1 flex flex-col gap-2 content-end">
+    <h4 className='flex justify-start text-md text-white font-semibold'>subscribe</h4>
+    <div className="flex-1 flex flex-row gap-3.5 flex-wrap">
+    <input placeholder="email" className="flex-1 ring-white ring-2 py-2.5 px-1.5 rounded-md"></input>
+    <button className=" px-2.5 py-1 bg-orange-400 text-white font-semibold text-lg rounded-md">submit</button>
     </div>
-    <div className="self-stretch flex justify-center items-center gap-3.5 flex-wrap content-center">
-      <input className="w-60 h-16 rounded-[10px] border-2 border-white"/>
-      <div className="px-6 py-3 bg-orange-400 rounded-[10px] flex justify-center items-center gap-2.5">
-        <div className="justify-start text-white text-xl font-bold font-['Lora']">submit</div>
-      </div>
-    </div>
+
   </div>
+    </div>
+ 
+ 
 </footer>
   )
 }
