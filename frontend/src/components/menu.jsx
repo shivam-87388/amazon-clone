82dd6f50fc0ddb66e8b2475f5f79f34acb43b7b9
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-02 12:36:51 +0530

    add

--- frontend/src/components/menu.jsx
+++ frontend/src/components/menu.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const menu = () => {
   return (
-    <header>menu</header>
+    <header class="w-h px-4 py-7 bg-white inline-flex flex-col justify-start items-start gap-2.5 overflow-hidden">
+  <div class="self-stretch pr-12 inline-flex justify-between items-center">
+    <img class="w-24 h-24" src="https://placehold.co/90x94" />
+    <div data-property-1="tabler-icon-menu-2" class="h-6 px-3 py-1 bg-teal-800 rounded-[3px] inline-flex flex-col justify-center items-center gap-2.5">
+      <div class="w-4 h-3 outline-1 outline-white"></div>
+    </div>
+  </div>
+</header>
   )
 }
 
