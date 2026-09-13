51438e6478e3ac5ca9b695789c3b75e11aa4b0b6
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-13 19:10:30 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -4,11 +4,9 @@ import React from 'react'
 const login = () => {
   return (
     <div className="flex items-center justify-center">
+    <div className=" bg-[url('/rectangle.png')] object-fill w-103.75 h-135.25 bg-cover bg-no-repeat">
+    <form className="flex flex-col items-center justify-center gap-2.5 ">
        <h1 className="text-center text-white text-2xl font-bold font-['Merriweather']">Login form</h1>
-    <div  className="flex flex-col bg-[url('/rectangle.png')] object-fill w-103.75 h-135.25 bg-cover bg-no-repeat items-center justify-center bg-center">
-   
-    
-    <form className="flex flex-col items-center justify-center gap-4">
      {/* email section */}
      <div className="flex flex-col gap-0.5">
      <label htmlFor="email" className="text-white text-[16px] font-normal font-['Merriweather']">email</label>
@@ -19,15 +17,9 @@ const login = () => {
      <label htmlFor="password" className="text-white text-[16px] font-normal font-['Merriweather']">Password</label>
     <input id="password" placeholder="password" className=" text-white text-[16px] w-64 px-2 py-1 rounded-md border-2 border-white" />
     </div>
-    <button className="w-40 px-3.5 py-2 bg-teal-500 rounded-lg flex justify-center items-center text-white text-base font-semibold font-['Merriweather']">login
-      
-
-    </button>
     </form>
-    
-    
-   
-   
+    <button className="w-32 px-3.5 py-2 bg-teal-800 rounded-lg flex justify-center items-center text-white text-base font-semibold font-['Merriweather']">login
+    </button>
     </div>
   
     </div>
