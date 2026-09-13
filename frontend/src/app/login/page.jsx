8f65e0a16dbd5d963c707a062143314001514598
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-13 18:24:05 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -1,32 +1,21 @@
+'use client'
 import React from 'react'
 
 const login = () => {
   return (
     <div className="flex items-center justify-center">
-      <div className="w-96 h-[541px] px-4 py-6 inline-flex flex-col justify-center items-center gap-2.5">
-  <div className="text-center justify-start text-white text-2xl font-bold font-['Merriweather']">login form</div>
-  <div className="self-stretch px-8 flex flex-col justify-start items-start gap-2.5">
-    <div className="self-stretch flex flex-col justify-start items-start gap-1.5">
-      <div className="self-stretch h-14 flex flex-col justify-start items-start gap-1">
-        <div className="self-stretch justify-start text-black text-base font-normal font-['Merriweather']">Email</div>
-        <div className="self-stretch px-2 py-1 rounded-lg outline outline-1 outline-offset-[-1px] outline-white inline-flex justify-start items-center gap-2.5">
-          <div className="text-center justify-start text-white text-base font-normal font-['Merriweather']">Ram@gamil.com</div>
-        </div>
-      </div>
-      <div className="self-stretch h-14 flex flex-col justify-start items-start gap-1">
-        <div className="self-stretch justify-start text-black text-base font-normal font-['Merriweather']">Password</div>
-        <div className="self-stretch px-2 py-1 rounded-lg outline outline-1 outline-offset-[-1px] outline-white inline-flex justify-start items-center gap-2.5">
-          <div className="text-center justify-start text-white text-base font-normal font-['Merriweather']">Password</div>
-        </div>
-      </div>
+    <div  className="flex flex-col bg-[url('/rectangle.png')] object-fill w-103.75 h-135.25 bg-cover bg-no-repeat items-center justify-center bg-center">
+    <h1 className="text-center text-white text-2xl font-bold font-['Merriweather']">Login form</h1>
+    
+    <div className="flex flex-col gap-0.5">
+     <label htmlFor="email" className="text-white text-base font-normal font-['Merriweather']">email</label>
+    <input className="w-64 px-2 py-1 rounded-md border-2 border-white" />
     </div>
-    <div className="self-stretch flex flex-col justify-center items-center gap-2.5">
-      <div className="px-3.5 py-2 bg-teal-500 rounded-lg inline-flex justify-center items-center gap-2.5">
-        <div className="justify-start text-white text-base font-semibold font-['Merriweather']">login</div>
-      </div>
+    
+    <label>password</label>
+    <input />
     </div>
-  </div>
-</div>
+  
     </div>
     
   )
