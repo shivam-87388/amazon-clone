b6cfbf82a280375fe92c94318649356981df41cb
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-14 23:33:27 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -4,10 +4,17 @@ import React from 'react'
 const login = () => {
   return (
  <div className="w-full h-screen p-4 bg-white flex justify-center items-center">
-  <form className="bg-[url('/rectangle.png')] bg-center bg-no-repeat bg-cover p-4 flex justify-center items-center gap-2.5">
+  <form className="bg-[url('/rectangle.png')] bg-center bg-no-repeat bg-cover p-4 flex flex-col justify-center items-center gap-2.5">
  <h1 className="text-center text-white text-2xl font-bold font-['Merriweather']">login form</h1>
 
- <div className="">
+ <div className=" w-full flex flex-col items-center justify-center">
+  {/* email section */}
+  <div className="flex flex-col">
+    <label>email</label>
+    <input/>
+  </div>
+ 
+
 
  </div>
  
