63cce63ba2f4c7297edc08b3eee2cea80a948ed4
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-14 23:52:48 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -10,7 +10,7 @@ const login = () => {
  <div className="flex flex-col items-center justify-center">
   {/* email section */}
   <div className="flex flex-col items-start justify-center">
-    <label htmlFor="email" className="text-white text-base font-normal font-['Merriweather']">email</label>
+    <label htmlFor="email" className="text-white text-base font-normal font-['Merriweather'] px-1">email</label>
     <input id='email' placeholder="email" className="border-2 border-white px-2 py-1 rounded-md"/>
   </div>
  
