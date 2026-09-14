4fb3a3d19f376487f27c1d71e04516a12c3c76c6
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-14 23:37:17 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -7,11 +7,11 @@ const login = () => {
   <form className="bg-[url('/rectangle.png')] bg-center bg-no-repeat bg-cover p-4 flex flex-col justify-center items-center gap-2.5">
  <h1 className="text-center text-white text-2xl font-bold font-['Merriweather']">login form</h1>
 
- <div className=" w-full flex flex-col items-center justify-center">
+ <div className="flex flex-col items-center justify-center">
   {/* email section */}
-  <div className="flex flex-col">
-    <label>email</label>
-    <input/>
+  <div className="flex flex-col items-start justify-center">
+    <label htmlFor="email" className="text-white text-base font-normal font-['Merriweather']">email</label>
+    <input id='email' placeholder="email" className="border-2 border-white"/>
   </div>
  
 
