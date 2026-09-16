489e5cb8bdb17e237e66e8d1a78464fcc958d328
shivam-87388 <hardwellshivamsingh2002@gmail.com>
2026-09-16 11:51:50 +0530

    add

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -38,10 +38,7 @@ const login = () => {
 
   return (
  <div className="w-full min-h-screen p-4 bg-white flex justify-center items-center">
-  <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
+  
   <form onSubmit={loginform.handleSubmit} className="bg-[url('/rectangle.png')] bg-center bg-no-repeat bg-cover px-10 py-4 flex flex-col justify-center items-center gap-4">
  <h1 className="text-center text-white text-2xl font-bold font-['Merriweather']">login form</h1>
 
@@ -49,16 +46,22 @@ const login = () => {
   {/* email section */}
   <div className="w-full flex flex-col items-start justify-center">
     <label htmlFor="user_email" className="text-white text-base font-normal font-['Merriweather'] px-1">Email</label>
-    <input type="email" id='user_email' value={loginform.values.user_email} onChange={loginform.handleChange} placeholder="email" className="font-['Merriweather'] w-full focus:ring-2 focus:ring-blue-500 text-white border-2 outline-hidden border-white px-2 py-1 rounded-md"/>
+    <input type="email" id='user_email' value={loginform.values.user_email} onBlur={loginform.handleBlur} onChange={loginform.handleChange} required placeholder="email" className="font-['Merriweather'] w-full focus:ring-2 focus:ring-blue-500 text-white border-2 outline-hidden border-white px-2 py-1 rounded-md"/>{loginform.touched.user_email && loginform.errors.user_email && (
+  <span className="text-black text-sm">
+    {loginform.errors.user_email}
+  </span>
+)}
   </div>
   {/* password section */}
   <div className="w-full flex flex-col ">
     <label htmlFor="user_password" className="text-white text-base font-normal font-['Merriweather'] px-1">Password</label>
     <div className="w-full border-2 border-white flex flex-row items-center rounded-md px-2 py-1 focus-within:ring-2 focus-within:ring-blue-500 gap-1">
-      <input value={loginform.values.user_password} onChange={loginform.handleChange} type={(show) ? "text":"password"}  id="user_password" placeholder="password" className="text-white outline-hidden font-['Merriweather']"/>
+      <input value={loginform.values.user_password} onChange={loginform.handleChange} type={(show) ? "text":"password"}  id="user_password" placeholder="password" required className="text-white outline-hidden font-['Merriweather']"/>
       <button type="button" className="hover:cursor-pointer" onClick={handleClick}>{(show)? <Eye color='white'/>:<EyeOff color='white'/>}
         </button>
-      </div> 
+        
+      </div>
+       
   </div>
  </div>
  <button type="submit" className="py-2.5 px-12 text-center text-base font-semibold font-['Merriweather'] hover:cursor-pointer bg-teal-800 text-white rounded-md">login</button>
